aa2c2c6be3ace63147e07d720f208788333fe285
mubarok <idhammubarok43@gmail.com>
2020-05-14 20:05:26 +0700

    add template

---
+++ navbar.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <a class="navbar-brand text-white" href="#">Sewa Mobil</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ">
+      <li class="nav-item active">
+        <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Kelola User</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Kelola Data Mobil</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Transaksi
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Sewa</a>
+          <a class="dropdown-item" href="#">Pengembalian</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Laporan</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav float-right">
+      <li class="nav-item">
+        <a class="nav-link text-white" href="index.php">Log Out</a>
+      </li>
+    </ul>
+  </div>
+</nav>
